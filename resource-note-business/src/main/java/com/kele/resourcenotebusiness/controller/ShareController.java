79b3cdfce55f01af8489e3d77693219e0b9b60b1
gaole <1033727528@qq.com>
2020-08-03 09:17:36 +0800

    使用 Spring Cloud 自带的服务发现进行服务与服务之间的访问

--- resource-note-business/src/main/java/com/kele/resourcenotebusiness/controller/ShareController.java
+++ resource-note-business/src/main/java/com/kele/resourcenotebusiness/controller/ShareController.java
@@ -9,11 +9,15 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * <p>
@@ -30,15 +34,23 @@ public class ShareController {
 
     private @NonNull
     final IShareService shareService;
-    private @NonNull RestTemplate restTemplate;
+    private @NonNull
+    final RestTemplate restTemplate;
+    private @NonNull
+    final DiscoveryClient discoveryClient;
 
 
     @GetMapping("{id}")
     public ShareDTO findById(@PathVariable Long id) {
         Share share = shareService.getById(id);
 
+        List<ServiceInstance> instances = discoveryClient.getInstances("mini-resource-user");
+        String url = instances.stream().map(instance -> instance.getUri().toString() + "/user/1")
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("当前实例不存在"));
+
         UserDTO userDTO = restTemplate.getForObject(
-                "http://localhost:29001/user/1",
+                url,
                 UserDTO.class
         );
 
